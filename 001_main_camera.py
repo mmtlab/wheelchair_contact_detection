6a7c936a5816a3b3273cbb164e14e5cb89258b52
giampai <g.busi006@studenti.unibs.it>
2023-06-02 12:31:21 +0200

    Fixed bugs on timestamp for each frame in 001

--- 001_main_camera.py
+++ 001_main_camera.py
@@ -14,15 +14,18 @@ import basic
 timer=basic.timer.Timer(name="001_main_camera")
 
 #full path of the file expected
-filecompletename=r'D:\01_raw\T017S03BnrC3r.bag' 
+#filecompletename=r"G:\Download\T129Subject13S2R1YR_run0-001.bag"
+filecompletename=r'C:\Users\Alexk\OneDrive\Documenten\School\RUG\jaar 3 bewegingswetenschappen(2022-2023)\BAP\Bag-files\T075Subject7S2R1NR_run0.bag'
+#filecompletename=r"C:\RUG\premaster\semester 2\BAP\oefendata\pilot_test2_s1r1nr.bag"
+#filecompletename=r"D:\TR3.bag"
 #usually 20000 frames for a full acquisition
 number_of_frames=20000
 x_resolution=848
 y_resolution=480
 threshold=100
-distance=1200
-SHOWVIDEO=True
+distance=1450
 led_status_lst=[]
+time=[]
 timestamp=[]
 rgblist=[]
 lm_lst=[]
@@ -32,12 +35,16 @@ hand_cyl_coord_lst=[]
 centre_metric_avg=np.array([0,0,0])
 handrimPlane_avg=np.array([0,0,0,0])
 hand_coordinates_camera_lst=[]
-video_full_path=r"D:\01_raw\video.avi"
-dir_lm=r'G:\Drive condivisi\Wheelchair Ergometer\handrim contact detection\Tests\20230309\02_preprocessing\realsenseRight\landmark'
-dir_handposition=r'G:\Drive condivisi\Wheelchair Ergometer\handrim contact detection\Tests\20230309\02_preprocessing\realsenseRight\handposition'
+dir_lm=r'G:\.shortcut-targets-by-id\1MjZH9XN1gWNoiyVyaLwpjTjfKx4AsRiw\handrim contact detection\Tests\20230519\02_preprocessing\realsenseRight\landmark'
+#dir_lm=r'C:\RUG\premaster\semester 2\BAP\oefendata\landmarks'
+# dir_lm=r'C:/Users/Alexk/OneDrive/Documenten/School/RUG/jaar 3 bewegingswetenschappen(2022-2023)/BAP/Landmarks'
+#dir_handposition=r'C:\RUG\premaster\semester 2\BAP\oefendata\handposition'
+dir_handposition=r'G:\.shortcut-targets-by-id\1MjZH9XN1gWNoiyVyaLwpjTjfKx4AsRiw\handrim contact detection\Tests\20230519\02_preprocessing\realsenseRight\handposition'
+# dir_handposition=r'C:/Users/Alexk/OneDrive/Documenten/School/RUG/jaar 3 bewegingswetenschappen(2022-2023)/BAP/Handposition'
+#dir_handposition=r'D:'
 # TODO one line loops to write the header
 header_lm=["time","x0","y0","x1","y1","x2","y2","x3","y3","x4","y4","x5","y5","x6","y6","x7","y7","x8","y8","x9","y9","x10","y10","x11","y11","x12","y12","x13","y13","x14","y14","x15","y15","x16","y16","x17","y17","x18","y18","x19","y19","x20","y20"]
-header_hand_position=["time","RadDistance[m]","RadAngle[rad]","NormDistance[m]"]
+header_hand_position=["time","RadDistance[mm]","RadAngle[rad]","NormDistance[mm]"]
 
 #start the pipeline and setup the configuration to read the .bag file
 pipeline = rs.pipeline()
@@ -66,6 +73,10 @@ for i in range(number_of_frames):
     #get the timestamp of each single frame
     timestamp_s = frame.get_timestamp()/1000
     timestamp.append(timestamp_s)
+    if i==0:
+        time.append(0)
+    if i!=0:
+        time.append(timestamp[i]-timestamp[i-1]+time[i-1])
     # get the depth and color frames
     depth_frame = frame.get_depth_frame()
     color_frame = frame.get_color_frame()
@@ -80,8 +91,9 @@ for i in range(number_of_frames):
         ppy = camera_intrinsics.ppy
         fx = camera_intrinsics.fx
         fy = camera_intrinsics.fy
-        wc_img, hrc_img, centre_metric, handrimPlane, dataPlane = hppdWC.analysis.findWheelCentreAndHandrim(color_image_rgb,depth_image,ppx,ppy,fx,fy)
-        
+        #wc_img, hrc_img, centre_metric, handrimPlane, dataPlane = hppdWC.analysis.findWheelCentreAndHandrim(color_image_rgb,depth_image,ppx,ppy,fx,fy)
+        #
+        wc_img, hrc_img, centre_metric, handrimPlane, dataPlane = hppdWC.analysis.findHandrim(color_image_rgb,depth_image,ppx,ppy,fx,fy,472,304)
         #update the average of the wheel centre coordinates and of the handrim plane at each iteration
         centre_metric_avg=(i*centre_metric_avg+np.array(centre_metric))/(i+1)
         handrimPlane_avg=(i*handrimPlane_avg+np.array([handrimPlane.a,handrimPlane.b,handrimPlane.c,handrimPlane.d]))/(i+1)
@@ -94,7 +106,7 @@ for i in range(number_of_frames):
     #get the hand landmarks and stores them in lm_lst for each frame with a different configuration for better csv writing
     hand_lm=coordinates.get_hand_landmarks(color_image_rgb,x_resolution,y_resolution)
     hand_lm_buffer=coordinates.multi_array_to_mono_array(hand_lm)
-    hand_lm_buffer.insert(0,timestamp_s)
+    hand_lm_buffer.insert(0,time[i])
     lm_lst.append(hand_lm_buffer)
     #get the hand position
     xh,yh,zh=coordinates.roi_position(hand_lm,depth_image)
@@ -102,6 +114,7 @@ for i in range(number_of_frames):
     xh,yh,zh=hppdWC.geom.convert_depth_pixel_to_metric_coordinate_pp_ff(zh, xh, yh, ppx, ppy, fx, fy)
     hand_coordinates_camera=[xh,yh,zh]
     hand_coordinates_camera_lst.append(hand_coordinates_camera)
+    print(i)
 timer.lap("time to find the hand landmark and roi position")
 #transform the hand coordinates to the wheel plane frame
 for j in range(len(hand_coordinates_camera_lst)):
@@ -111,10 +124,10 @@ for j in range(len(hand_coordinates_camera_lst)):
         hand_coordinates_hrplane=coordinates.changeRefFrameTR(hand_coordinates_camera_lst[j], centre_metric_avg, rot)
         #transform the hand coordinates from cartesian to cylindrical
         hand_cyl_coordinates=coordinates.from_cart_to_cylindrical_coordinates(hand_coordinates_hrplane)
-        hand_cyl_coordinates.insert(0,timestamp[j])
+        hand_cyl_coordinates.insert(0,time[j])
         hand_cyl_coord_lst.append(hand_cyl_coordinates)
     else:
-        hand_cyl_coord_lst[j].insert(0,timestamp[j])
+        hand_cyl_coord_lst[j].insert(0,time[j])
         hand_cyl_coord_lst.append(np.nan)
         # TODO check if nan is fine or requires list of three elements
 #saves the landmark list for each frame to a csv file in the current project directory
